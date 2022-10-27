f56b2544499047d81b99d5c7f7f904577055c850
user <user@DESKTOP-ER86RLF>
2022-10-27 10:44:43 +0530

    Pushing code first time.

---
+++ SeptBatch_API/src/test/java/Sept9PMBatch_API/SeptBatch_API/GetRequest.java
@@ -0,0 +1,23 @@
+package Sept9PMBatch_API.SeptBatch_API;
+
+import static io.restassured.RestAssured.*;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+public class GetRequest 
+{
+	public static void main(String[] args)
+	{
+		Response res=
+	given()
+	.contentType(ContentType.JSON)
+	.when()
+	.get("http://localhost:3000/Seleniumstudents?firstname=Ranjita");
+		System.out.println("Status code is:"+ res.statusCode());
+		System.out.println("Response data is");
+		System.out.println(res.asString());
+		
+	}
+	
+}
