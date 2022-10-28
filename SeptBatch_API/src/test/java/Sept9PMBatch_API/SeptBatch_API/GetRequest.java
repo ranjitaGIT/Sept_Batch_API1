e5745023f0a9e2e13d20840cdbb7c8b7219c6af2
user <user@DESKTOP-ER86RLF>
2022-10-28 09:57:35 +0530

    pushing 1st-code

--- SeptBatch_API/src/test/java/Sept9PMBatch_API/SeptBatch_API/GetRequest.java
+++ SeptBatch_API/src/test/java/Sept9PMBatch_API/SeptBatch_API/GetRequest.java
@@ -18,7 +18,8 @@ public class GetRequest
 		System.out.println("Response data is");
 		System.out.println(res.asString());
 		System.out.println("PUSH&COMMIT");
-		System.out.println("PUSH&COMMIT@@");
+		System.out.println("PUSH&COMMIT222");
+		
 	}
 	
 }
